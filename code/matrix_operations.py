da105678a9304d26beb306f7f9a4d7dd69a22f99
jovyan <jovanprodanov123@gmail.com>
2024-01-17 15:15:52 +0100

    add ray function wrapper which executes on best suitable node

--- code/matrix_operations.py
+++ code/matrix_operations.py
@@ -1,22 +1,16 @@
-import ray
-
 import numpy as np
 
-ray.init("ray://10.1.33.199:10001")
-print(ray.available_resources())
+from wrapper import execute_on_best_pod
 
 
-@ray.remote(num_cpus=4, resources={"node:10.1.245.2": 1.0})
-def matrix_op(matrix_a, matrix_b):
-    result_matrix = np.dot(matrix_a, matrix_b)
+def matrix_op(matrix_size):
+    result_matrix = np.dot(np.random.rand(matrix_size, matrix_size), np.random.rand(matrix_size, matrix_size))
     determinant = np.linalg.det(result_matrix)
     eigenvalues, eigenvectors = np.linalg.eig(result_matrix)
     inverse_matrix = np.linalg.inv(result_matrix)
     return inverse_matrix
 
 
-matrix_size = 1000
-matrix = np.random.rand(matrix_size, matrix_size)
-ray.get(matrix_op.remote(matrix, matrix))
-
-ray.shutdown()
+if __name__ == '__main__':
+    m_size = 1000
+    execute_on_best_pod(matrix_op, m_size)
