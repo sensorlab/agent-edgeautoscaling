0f573dfe906ee449251ef674dc3c7b277752af1c
wrathchild14 <jovanprodanov123@gmail.com>
2024-02-14 15:29:04 +0100

    improve saving data

--- code/node.py
+++ code/node.py
@@ -1,6 +1,6 @@
-from datetime import datetime
-
 import requests
+
+from datetime import datetime
 from kubernetes import client, config
 
 
@@ -37,7 +37,7 @@ class Node:
         return self.containers
 
     def get_container_usage(self, container_id):
-        containers_stats_url = f"http://{self.ca_ip}:8080/api/v1.3/subcontainers/kubepods/burstable/"
+        containers_stats_url = f"http://{self.ca_ip}:8080/api/v1.3/subcontainers/kubepods/"
 
         response = requests.get(containers_stats_url)
         if response.status_code == 200:
@@ -69,6 +69,70 @@ class Node:
                 memory_limit_bytes = container["spec"]["memory"]["limit"]
                 memory_usage_percentage = (current_memory_usage_bytes / memory_limit_bytes) * 100
 
+                # network_rx_per_second_mb, network_tx_per_second_mb = self.get_throughput(time_interval_seconds)
+
+                return (cpu_limit_mc, cpu_usage_millicores, cpu_usage_percentage), (memory_limit_bytes, memory_usage_megabytes, memory_usage_percentage)
+            else:
+                print(f"Container {container_id} not found")
+                return (0, 0, 0), (0, 0, 0), (0, 0), (0, 0)
+        else:
+            print("Failed to fetch containers stats")
+
+    def get_container_usage_saving_data(self, container_id):
+        containers_stats_url = f"http://{self.ca_ip}:8080/api/v1.3/subcontainers/kubepods/"
+
+        response = requests.get(containers_stats_url)
+        if response.status_code == 200:
+            containers_stats = response.json()
+            container = next((c for c in containers_stats if container_id in c["name"]), None)
+            if container:
+                current_cpu_usage_nanoseconds = container["stats"][-1]["cpu"]["usage"]["total"]
+                previous_cpu_usage_nanoseconds = container["stats"][-2]["cpu"]["usage"]["total"]
+
+                current_timestamp_str = container["stats"][-1]["timestamp"].split('.')[0] + 'Z'
+                previous_timestamp_str = container["stats"][-2]["timestamp"].split('.')[0] + 'Z'
+
+                current_timestamp = datetime.strptime(current_timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
+                previous_timestamp = datetime.strptime(previous_timestamp_str, "%Y-%m-%dT%H:%M:%SZ")
+
+                time_interval = current_timestamp - previous_timestamp
+                time_interval_seconds = time_interval.total_seconds()
+
+                cpu_usage_delta_nanoseconds = current_cpu_usage_nanoseconds - previous_cpu_usage_nanoseconds
+                cpu_usage_per_second = cpu_usage_delta_nanoseconds / time_interval_seconds
+
+                cpu_usage_millicores = cpu_usage_per_second / 1000000
+                cpu_limit_mc = container["spec"]["cpu"]["limit"]
+                cpu_usage_percentage = (cpu_usage_per_second / (cpu_limit_mc * 1_000_000)) * 100
+
+                current_memory_usage_bytes = container["stats"][-1]["memory"]["usage"]
+
+                memory_usage_megabytes = current_memory_usage_bytes / (1024 * 1024)
+                memory_limit_bytes = container["spec"]["memory"]["limit"]
+                memory_usage_percentage = (current_memory_usage_bytes / memory_limit_bytes) * 100
+
+                memory_usage_cache = container["stats"][-1]["memory"]["cache"]
+                memory_usage_rss = container["stats"][-1]["memory"]["rss"]
+                memory_usage_swap = container["stats"][-1]["memory"]["swap"]
+                memory_usage_mapped_file = container["stats"][-1]["memory"]["mapped_file"]
+                memory_usage_working_set = container["stats"][-1]["memory"]["working_set"]
+                previous_memory_usage_cache = container["stats"][-2]["memory"]["cache"]
+                previous_memory_usage_rss = container["stats"][-2]["memory"]["rss"]
+                previous_memory_usage_swap = container["stats"][-2]["memory"]["swap"]
+                previous_memory_usage_mapped_file = container["stats"][-2]["memory"]["mapped_file"]
+                previous_memory_usage_working_set = container["stats"][-2]["memory"]["working_set"]
+                memory_usage_cache_delta = memory_usage_cache - previous_memory_usage_cache
+                memory_usage_rss_delta = memory_usage_rss - previous_memory_usage_rss
+                memory_usage_swap_delta = memory_usage_swap - previous_memory_usage_swap
+                memory_usage_mapped_file_delta = memory_usage_mapped_file - previous_memory_usage_mapped_file
+                memory_usage_working_set_delta = memory_usage_working_set - previous_memory_usage_working_set
+
+                mem_usage_cache_sec = memory_usage_cache_delta / time_interval_seconds
+                mem_usage_rss_sec = memory_usage_rss_delta / time_interval_seconds
+                mem_usage_swap_sec = memory_usage_swap_delta / time_interval_seconds
+                mem_usage_mapped_file_sec = memory_usage_mapped_file_delta / time_interval_seconds
+                mem_usage_working_set_sec = memory_usage_working_set_delta / time_interval_seconds
+
                 io_read_curr = container["stats"][-1]["diskio"]["io_service_bytes"][0]['stats']['Read']
                 io_read_prev = container["stats"][-2]["diskio"]["io_service_bytes"][0]['stats']['Read']
                 io_delta = io_read_curr - io_read_prev
@@ -79,12 +143,32 @@ class Node:
                 io_delta = io_write_curr - io_write_prev
                 io_write_per_second = io_delta / time_interval_seconds
                 
-                return (cpu_limit_mc, cpu_usage_millicores, cpu_usage_percentage), (memory_limit_bytes, memory_usage_megabytes, memory_usage_percentage), (io_read_per_second, io_write_per_second)
+                network_rx_per_second_mb, network_tx_per_second_mb = self.get_throughput(time_interval_seconds)
+
+                return (cpu_limit_mc, cpu_usage_millicores, cpu_usage_percentage), (memory_limit_bytes, memory_usage_megabytes, memory_usage_percentage), (io_read_per_second, io_write_per_second), (network_rx_per_second_mb, network_tx_per_second_mb), (mem_usage_cache_sec, mem_usage_rss_sec, mem_usage_swap_sec, mem_usage_mapped_file_sec, mem_usage_working_set_sec)
             else:
                 print(f"Container {container_id} not found")
-                return 0, 0, 0, 0
+                return (0, 0, 0), (0, 0, 0), (0, 0), (0, 0)
         else:
             print("Failed to fetch containers stats")
+
+    def get_throughput(self, time_interval):
+        containers_url = f"http://{self.ca_ip}:8080/api/v1.3/containers"
+        response = requests.get(containers_url)
+        if response.status_code == 200:
+            containers_stats = response.json()
+            # todo: fixme
+            current_network_rx_bytes = containers_stats["stats"][-1]["network"]["interfaces"][-2]["rx_bytes"]
+            previous_network_rx_bytes = containers_stats["stats"][-2]["network"]["interfaces"][-2]["rx_bytes"]
+            network_rx_delta = current_network_rx_bytes - previous_network_rx_bytes
+            network_rx_per_second = network_rx_delta / time_interval
+
+            current_network_tx_bytes = containers_stats["stats"][-1]["network"]["interfaces"][-2]["tx_bytes"]
+            previous_network_tx_bytes = containers_stats["stats"][-2]["network"]["interfaces"][-2]["tx_bytes"]
+            network_tx_delta = current_network_tx_bytes - previous_network_tx_bytes
+            network_tx_per_second = network_tx_delta / time_interval
+
+            return (network_rx_per_second / (1024 * 1024)), (network_tx_per_second / (1024 * 1024))
 
     def get_usage(self):
         summary_url = f"http://{self.ca_ip}:8080/api/v2.0/summary"
@@ -110,6 +194,24 @@ class Node:
         else:
             print("Failed to fetch resource usage.")
 
+    def get_means(self):
+        summary_url = f"http://{self.ca_ip}:8080/api/v2.0/summary"
+
+        response = requests.get(summary_url)
+        if response.status_code == 200:
+            summary_data = response.json()
+
+            minute_cpu = summary_data.get("/", {}).get("minute_usage", {}).get("cpu")['mean']
+            hour_cpu = summary_data.get("/", {}).get("hour_usage", {}).get("cpu")['mean']
+            day_cpu = summary_data.get("/", {}).get("day_usage", {}).get("cpu")['mean']
+            minute_memory = summary_data.get("/", {}).get("minute_usage", {}).get("memory")['mean']
+            hour_memory = summary_data.get("/", {}).get("hour_usage", {}).get("memory")['mean']
+            day_memory = summary_data.get("/", {}).get("day_usage", {}).get("memory")['mean']
+
+            return (minute_cpu, hour_cpu, day_cpu), (minute_memory, hour_memory, day_memory)
+        else:
+            print("Failed to fetch resource usage.")
+
     def get_node_capacity(self):
         limits_url = f"http://{self.ca_ip}:8080/api/v2.0/machine"
         response = requests.get(limits_url)
@@ -125,8 +227,8 @@ class Node:
         allocated_cpu = 0
         allocated_memory = 0
         for container_id, _ in list(self.get_containers().items()):
-            containers_stats_url = f"http://{self.ca_ip}:8080/api/v1.3/subcontainers/kubepods/burstable/"
-            response = requests.get(containers_stats_url)
+            containers_url = f"http://{self.ca_ip}:8080/api/v1.3/subcontainers/kubepods/"
+            response = requests.get(containers_url)
             if response.status_code == 200:
                 containers_stats = response.json()
                 container = next((c for c in containers_stats if container_id in c["name"]), None)
@@ -135,7 +237,7 @@ class Node:
                     allocated_memory += container['spec']['memory']['limit']
         return allocated_cpu, allocated_memory
 
-    # wip: doesn't calculate accurately (just calculates how much the ray pods have allocated)
+    # wip: calculates how much the pods from the application have allocated
     def get_unallocated_capacity(self):
         total_cpu_capacity, total_memory_capacity = self.get_node_capacity()
         allocated_cpu, allocated_memory = self.get_allocated_resources()
@@ -144,3 +246,20 @@ class Node:
         unallocated_memory = max(0, (total_memory_capacity - allocated_memory) / (1024 * 1024))
 
         return unallocated_cpu, unallocated_memory
+
+    def get_root_storage(self):
+        filesystem_url = f"http://{self.ca_ip}:8080/api/v2.0/storage"
+        response = requests.get(filesystem_url)
+        if response.status_code == 200:
+            filesystem_stats = response.json()
+            filesystem = next((fs for fs in filesystem_stats if fs['mountpoint'] == '/'), None)
+            if filesystem:
+                used = filesystem['usage']
+                limit = filesystem['capacity']
+                available = filesystem['available']
+                percentage = (used / limit) * 100
+                return ((used / (1024 * 1024 * 1024)), (limit / (1024 * 1024 * 1024)), (available / (1024 * 1024 * 1024)), percentage)
+            else:
+                print("Failed to fetch filesystem usage.")
+        else:
+            print("Failed to fetch filesystem usage.")
--- code/save_data.py
+++ code/save_data.py
@@ -2,7 +2,7 @@ import csv
 import datetime
 import time
 
-from utils import init_nodes
+from utils import init_nodes, load_config
 
 DEBUG = True
 timeseries_data = []
@@ -10,7 +10,45 @@ timeseries_data = []
 
 def write_to_csv(filename):
     with open(filename, mode='w', newline='') as file:
-        fieldnames = ["pod_name", "timestamp", "cpu_millicores", "memory_mb", "max_cpu", "max_memory_mb"]
+        fieldnames = [
+            "timestamp",
+            "node_name",
+            "node_cpu_usage_mc",
+            "node_cpu_usage_percentage",
+            "node_memory_usage_mb",
+            "node_memory_usage_percentage",
+            "node_free_cpu_mc",
+            "node_free_memory_mb",
+            "node_unallocated_cpu_mc",
+            "node_unallocated_memory_mb",
+            "node_max_cpu_mc",
+            "node_max_memory_mb",
+            "node_cpu_minute_mean_mc",
+            "node_memory_minute_mean_mb",
+            "node_cpu_hour_mean_mc",
+            "node_memory_hour_mean_mb",
+            "node_cpu_day_mean_mc",
+            "node_memory_day_mean_mb",
+            "node_root_storage_usage_gb",
+            "node_root_storage_limit_gb",
+            "node_root_storage_available_gb",
+            "node_root_storage_percentage_usage_gb",
+            "pod_name",
+            "pod_cpu_usage_mc",
+            "pod_cpu_limit_mc",
+            "pod_cpu_usage_percentage",
+            "pod_mem_usage_cache_b",
+            "pod_mem_usage_rss_b",
+            "pod_mem_usage_swap_b",
+            "pod_mem_usage_mapped_file_b",
+            "pod_memory_limit_mb",
+            "pod_memory_usage_mb",
+            "pod_memory_usage_percentage",
+            "pod_io_read_usage",
+            "pod_io_write_usage",
+            "pod_network_rx_mb",
+            "pod_network_tx_mb"
+        ]
         writer = csv.DictWriter(file, fieldnames=fieldnames)
 
         writer.writeheader()
@@ -19,28 +57,68 @@ def write_to_csv(filename):
 
 
 if __name__ == '__main__':
-    nodes = init_nodes(DEBUG)
+    config = load_config()
+    nodes = init_nodes(debug=config.get('DEBUG'), custom_label=config.get('custom_app_label'))
 
-    duration_seconds = 3
-    interval_seconds = 1
+    duration_seconds = 300
+    sampling_interval = 5
     file_name = 'timeseries.csv'
     end_time = time.time() + duration_seconds
 
     while time.time() < end_time:
+        start_time = time.time()
         for node in nodes:
+            node_cpu, node_cpu_percentage, node_memory, node_memory_percentage, free_cpu, free_mem = node.get_usage()
+            unallocated_cpu, unallocated_memory = node.get_unallocated_capacity()
+            max_cpu, max_memory = node.get_node_capacity()
+            (minute_cpu, hour_cpu, day_cpu), (minute_mem, hour_mem, day_mem) = node.get_means()
+            (root_usage, root_limit, root_available, root_percentage) = node.get_root_storage()
+
             for container_id, (pod_name, container_name, pod_ip) in list(node.get_containers().items()):
-                (_, cpu, cpu_p), (_ ,memory_mb, memory_p) = node.get_container_usage(container_id)
-                max_cpu, max_memory = node.get_node_capacity()  # todo, no need to get it every time
+                (cpu_limit, cpu, cpu_p), (mem_limit, mem, mem_p), (io_read, io_write), (rx, tx), (mem_usage_cache_sec, mem_usage_rss_sec, mem_usage_swap_sec, mem_usage_mapped_file_sec, mem_usage_working_set_sec) = node.get_container_usage_saving_data(container_id)
 
                 timeseries_data.append({
-                    "pod_name": pod_name,
                     "timestamp": datetime.datetime.now(),
-                    "cpu_millicores": cpu,
-                    "memory_mb": memory_mb,
-                    "max_cpu": max_cpu,
-                    "max_memory_mb": max_memory / (1024 * 1024),
+                    "node_name": node.name,
+                    "node_cpu_usage_mc": node_cpu,
+                    "node_cpu_usage_percentage": node_cpu_percentage,
+                    "node_memory_usage_mb": node_memory,
+                    "node_memory_usage_percentage": node_memory_percentage,
+                    "node_free_cpu_mc": free_cpu,
+                    "node_free_memory_mb": free_mem,
+                    "node_unallocated_cpu_mc": unallocated_cpu,
+                    "node_unallocated_memory_mb": unallocated_memory,
+                    "node_max_cpu_mc": max_cpu,
+                    "node_max_memory_mb": max_memory / (1024 * 1024),
+                    "node_cpu_minute_mean_mc": minute_cpu,
+                    "node_memory_minute_mean_mb": minute_mem / (1024 * 1024),
+                    "node_cpu_hour_mean_mc": hour_cpu,
+                    "node_memory_hour_mean_mb": hour_mem / (1024 * 1024),
+                    "node_cpu_day_mean_mc": day_cpu,
+                    "node_memory_day_mean_mb": day_mem / (1024 * 1024),
+                    "node_root_storage_usage_gb": root_usage,
+                    "node_root_storage_limit_gb": root_limit,
+                    "node_root_storage_available_gb": root_available,
+                    "node_root_storage_percentage_usage_gb": root_percentage,
+                    "pod_name": pod_name,
+                    "pod_cpu_usage_mc": cpu,
+                    "pod_cpu_limit_mc": cpu_limit,
+                    "pod_cpu_usage_percentage": cpu_p,
+                    "pod_memory_limit_mb": mem_limit / (1024 * 1024),
+                    "pod_memory_usage_mb": mem,
+                    "pod_memory_usage_percentage": mem_p,
+                    "pod_mem_usage_cache_b": mem_usage_cache_sec,
+                    "pod_mem_usage_rss_b": mem_usage_rss_sec,
+                    "pod_mem_usage_swap_b": mem_usage_swap_sec,
+                    "pod_mem_usage_mapped_file_b": mem_usage_mapped_file_sec,
+                    "pod_io_read_usage": io_read,
+                    "pod_io_write_usage": io_write,
+                    "pod_network_rx_mb": rx,
+                    "pod_network_tx_mb": tx,
                 })
-        time.sleep(interval_seconds)
+        elapsed_time = time.time() - start_time
+        if elapsed_time < sampling_interval:
+            time.sleep(sampling_interval - elapsed_time)
 
     write_to_csv(f"data/{file_name}")
-    print(f"Saved data to file: {file_name}, interval: {interval_seconds}s for {duration_seconds}s")
+    print(f"Saved data to file: {file_name}, interval: {sampling_interval}s for {duration_seconds}s")
