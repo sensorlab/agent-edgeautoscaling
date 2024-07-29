fix instant scaling bug, fix log latency metric bug

code/envs.py
@@ -233,7 +233,8 @@ class InstantContinuousElasticityEnv(BaseElasticityEnv):
         action = np.clip(action, self.action_space.low, self.action_space.high)
         scale_action = action[0] * self.MAX_CPU_LIMIT
 
-        if scale_action < self.ALLOCATED or scale_action <= max(self.AVAILABLE, 0):
+        # if scale_action < self.ALLOCATED or (self.ALLOCATED - scale_action) <= max(self.AVAILABLE, 0):
+        if (scale_action - self.ALLOCATED) <= max(self.AVAILABLE, 0):
             new_resource_limit = int(max(scale_action, self.MIN_CPU_LIMIT))
             self.ALLOCATED = new_resource_limit
             patch_pod(

code/train_mdqn.py
@@ -378,7 +378,6 @@ if __name__ == '__main__':
                 print()
         
             [agents_ep_reward[i].append(rewards[i]) for i in range(n_agents)]
-            [agents_mean_latenices[i].append(np.mean(latency)) for i, latency in enumerate(agents_ep_mean_latency)]
 
             ep_rewards += np.mean(rewards)
 
@@ -406,6 +405,7 @@ if __name__ == '__main__':
         summed_rewards.append(ep_rewards)
         
         [agents_summed_rewards[i].append(np.sum(reward)) for i, reward in enumerate(agents_ep_reward)]
+        [agents_mean_latenices[i].append(np.mean(latency)) for i, latency in enumerate(agents_ep_mean_latency)]
         print(f"Episode {i_episode} reward: {ep_rewards} mean latency: {np.mean(ep_latencies)}")
 
         spam_process.terminate()

code/train_ppo.py
@@ -463,10 +463,12 @@ if __name__ == "__main__":
             print(f"Resources changed to {RESOURCES} for episode {episode}")
 
         if episode % 4 == 0 and train_priority:
-            for env in envs:
-                env.priority = random.randint(1, 10) / 10.0
+            priorities = [random.randint(1, 10) / 10.0 for _ in range(n_agents)]
+            for i, env in enumerate(envs):
+                env.priority = priorities[i]
                 # if reset_env:
                 #     env.patch(100)
+            print(f"Priorities for envs changed to {priorities} for episode {episode}")
 
         # random_rps = np.random.randint(min_rps, reqs_per_second) if randomize_reqs else reqs_per_second
         # In this training the random rps is handled by the loading script