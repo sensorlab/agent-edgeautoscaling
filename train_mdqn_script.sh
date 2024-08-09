01736805199dda7ee00b98e96ea10480224002d0
wrathchild14 <jovanprodanov123@gmail.com>
2024-08-09 14:20:39 +0200

    remove bash train script

--- train_mdqn_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# calculate step latency with 10 rps
-python code/train_mdqn.py --episodes 300 --increment_action 50 --init_resources 1000 --rps 100
-python code/train_mdqn.py --episodes 300 --increment_action 25 --init_resources 500 --rps 50 --alpha 0.6
