22cb207ca3dce6bfc9df9e929753866052bfc093
wrathchild14 <jovanprodanov123@gmail.com>
2024-09-24 15:40:42 +0200

    add more apis to be

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ from envs import set_available_resource, set_other_priorities, set_other_utiliza
 class Application:
     def __init__(self):
         self.resources = 1000
-        self.debug = True
+        self.debug = False
         self.action_interval = 1
         self.current_algorithm = None
         self.set_ppo()
@@ -38,7 +38,7 @@ class Application:
                 rewards.append(reward)
                 dones.append(done)
                 if self.debug:
-                    print(f"{self.envs[i].id}: ACTION: {action}, LIMIT: {self.envs[i].ALLOCATED}, {self.envs[i].last_cpu_percentage:.2f}%, AVAILABLE: {self.envs[i].AVAILABLE}, reward: {reward} state(limit, usage, others): {self.envs[i].state[-1]}")
+                    print(f"{self.envs[i].id}: ACTION: {action}, LIMIT: {self.envs[i].ALLOCATED}, {self.envs[i].last_cpu_percentage: .2f}%, AVAILABLE: {self.envs[i].AVAILABLE}, reward: {reward} state(limit, usage, others): {self.envs[i].state[-1]}")
             if self.debug:
                 print()
             
@@ -75,7 +75,7 @@ class Application:
 
     def set_dqn(self):
         if self.current_algorithm != 'dqn':
-            self.envs, self.agents = initalize_agents(n_agents=3, resources=1000, tl_agent=None, model='trained/dqn/mdqn1000ep1000m25inc2_rf_20rps5.0alpha1000res', algorithm='mdqn', independent=False, priorities=[1.0, 1.0, 1.0])
+            self.envs, self.agents = initalize_agents(n_agents=3, resources=1000, tl_agent=2, model='trained/dqn/mdqn1000ep1000m25inc2_rf_20rps5.0alpha1000res', algorithm='mdqn', independent=False, priorities=[1.0, 1.0, 1.0])
             self.current_algorithm = 'dqn'
             return {"message": "DQN algorithm set"}
         else:
@@ -97,7 +97,10 @@ os.chdir(parent_dir)
 app = Application()
 elasticity_app = FastAPI()
 
-class Item(BaseModel):
+class IntervalRequest(BaseModel):
+    interval: int
+
+class ResourceRequest(BaseModel):
     resources: int
 
 @elasticity_app.post("/start")
@@ -109,14 +112,34 @@ def stop_inference():
     return app.stop_inference()
 
 @elasticity_app.post("/set_resources")
-def set_resources(item: Item):
+def set_resources(item: ResourceRequest):
     return app.set_resources(item.resources)
 
+@elasticity_app.post("/set_interval")
+def set_interval(request: IntervalRequest):
+    app.action_interval = request.interval
+    return {"message": f"Interval set to {request.interval}"}
+
 @elasticity_app.post("/set_dqn_algorithm")
-def set_resources():
+def set_dqn():
     return app.set_dqn()
 
 @elasticity_app.post("/set_ppo_algorithm")
-def set_resources():
+def set_ppo():
     return app.set_ppo()
 
+@elasticity_app.get("/status")
+def get_status():
+    return {"status": app.infer_thread.is_alive() if app.infer_thread is not None else False}
+
+@elasticity_app.get("/algorithm")
+def get_algorithm():
+    return {"algorithm": app.current_algorithm}
+
+@elasticity_app.get("/resources")
+def get_resources():
+    return {"resources": app.resources}
+
+@elasticity_app.get("/interval")
+def get_interval():
+    return {"interval": app.action_interval}
