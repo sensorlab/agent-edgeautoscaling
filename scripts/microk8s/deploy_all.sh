e162f3bd6a9d3699e77564c9ba22193dc6f49e9c
wrathchild14 <jovanprodanov123@gmail.com>
2024-11-27 12:42:18 +0100

    fix issue #4 in the deployment script

--- scripts/microk8s/deploy_all.sh
+++ scripts/microk8s/deploy_all.sh
@@ -8,10 +8,32 @@ log() {
 log "Setting up Kubernetes configuration"
 microk8s config > ~/.kube/config
 
-log "Labeling nodes for cluster identification"
-microk8s kubectl label nodes raspberrypi1 cluster=rasp1 --overwrite
-microk8s kubectl label nodes raspberrypi2 cluster=rasp2 --overwrite
-microk8s kubectl label nodes e6-orancloud cluster=vm --overwrite
+# log "Labeling nodes for cluster identification"
+# microk8s kubectl label nodes raspberrypi1 cluster=rasp1 --overwrite
+# microk8s kubectl label nodes raspberrypi2 cluster=rasp2 --overwrite
+# microk8s kubectl label nodes e6-orancloud cluster=vm --overwrite
+
+log "Labeling nodes for cluster identification, NOTE: Looking for substrings of the node names"
+
+read -p "INPUT the master node name or a substring: " master_node
+read -p "INPUT the substring of the edge nodes: " edge_nodes
+i=0
+nodes=$(microk8s kubectl get nodes -o jsonpath='{.items[*].metadata.name}')
+for node in $nodes; do
+    case $node in
+        $edge_nodes*)
+            microk8s kubectl label nodes $node cluster=rasp$((++i)) --overwrite
+            log "Labeling $node with cluster=rasp$((++i))"
+            ;;
+        $master_node*)
+            microk8s kubectl label nodes $node cluster=vm --overwrite
+            log "Labeling $node with cluster=vm"
+            ;;
+        *)
+            log "Unknown node: $node"
+            ;;
+    esac
+done
 
 log "Deploying cAdvisor for container monitoring"
 microk8s kubectl apply -f configs/cadvisor/config.yaml
@@ -29,7 +51,6 @@ microk8s kubectl create namespace metrics || true
 
 log "Installing Prometheus stack for monitoring"
 microk8s helm install stack prometheus-community/kube-prometheus-stack --namespace metrics --values configs/prometheus_stack/values.yaml
-
 
 log "Deploying localization services"
 microk8s kubectl apply -f configs/localization/separate_services.yaml
