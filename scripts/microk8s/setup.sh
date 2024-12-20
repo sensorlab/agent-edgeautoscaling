7476cb7d48b009e38208708799f37bd3cd130000
wrathchild14 <jovanprodanov123@gmail.com>
2024-12-20 14:51:44 +0100

    add input for swap to setup script

--- scripts/microk8s/setup.sh
+++ scripts/microk8s/setup.sh
@@ -18,6 +18,14 @@ fi
 log "Removing existing microk8s installation"
 sudo snap remove --purge microk8s
 
+read -p "Do you want to disable swap? (yes/no): " disable_swap
+if [ "$disable_swap" == "yes" ]; then
+    log "Disabling swap"
+    sudo swapoff -a
+else
+    log "Skipping swap disable"
+fi
+
 log "Installing microk8s"
 sudo snap install microk8s --classic --channel=1.28
 
